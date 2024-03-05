ff5b49c7ebcf6fa4d9a51157793972bf55e42bce
vishala-pc <107846242+vishala-pc@users.noreply.github.com>
2024-03-05 12:15:58 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name="ksdb_parse",
-    version="0.0.1",
+    name="actowiz_custom_proxy",
+    version="0.0.23",
     description="",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
-    long_description="KSDB Parse",
+    long_description="actowiz custom proxy",
     long_description_content_type="text/markdown",
     url="",
     author="VISHAL AGHERA",
@@ -17,7 +17,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    install_requires=["scrapy >= 2.11.0", ],
+    install_requires=["elasticsearch >= 8.11.1", "scrapy >= 2.11.0"],
     extras_require={
         "dev": ["twine>=4.0.2"],
     },
